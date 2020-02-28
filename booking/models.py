f97a590c146558e9f1da145fd4847da5781dce02
t <38640424+poohcid@users.noreply.github.com>
2020-02-28 11:47:18 +0700

    makemigration

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    open_time = models.TimeField()
+    end_time = models.TimeField()
+    capacity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
